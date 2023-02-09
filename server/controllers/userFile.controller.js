f56fa5b7246458ce066a73c5e1b9cb492f79d716
Zeyd Haigar <zeyd.haigar@gmail.com>
2023-02-09 17:12:10 +0100

    update user_file ok

--- server/controllers/userFile.controller.js
+++ server/controllers/userFile.controller.js
@@ -108,47 +108,47 @@ exports.update = (req, res) => {
 
         if (userIdParamsUrl == results[0].user_file_userId) {
           console.log("AUTORISATION POUR MODIF OBJET");
-        }
 
-        //vérification de la présence d'une image à modifier
-        if (req.file) {
-          //récupération du nom du fichier à supprimer dans la db
-          const filename = results[0].user_file_profilePic.split("/images")[1];
-          console.log("FILENAME");
-          console.log(filename);
+          //vérification de la présence d'une image à modifier
+          if (req.file) {
+            //récupération du nom du fichier à supprimer dans la db
+            const filename =
+              results[0].user_file_profilePic.split("/images")[1];
+            console.log("FILENAME");
+            console.log(filename);
 
-          //suppression du fichier dans dossier images
-          fs.unlink(`images/${filename}`, (error) => {
-            if (error) throw error;
-          });
-        }
+            //suppression du fichier dans dossier images
+            fs.unlink(`images/${filename}`, (error) => {
+              if (error) throw error;
+            });
+          }
 
-        //objet mis à jour dans db
-        console.log("CONTENU REQ.BODY");
-        console.log(req.body);
-        console.log("CONTENU REQ.BODY.USERFILE");
-        console.log(req.body.userFile);
+          //objet mis à jour dans db
+          console.log("CONTENU REQ.BODY");
+          console.log(req.body);
+          console.log("CONTENU REQ.BODY.USERFILE");
+          console.log(req.body.userFile);
 
-        const userFileObject = JSON.parse(req.body.userFile);
-        console.log("CONTENU USERFILEOBJECT");
-        console.log(userFileObject);
+          const userFileObject = JSON.parse(req.body.userFile);
+          console.log("CONTENU USERFILEOBJECT");
+          console.log(userFileObject);
 
-        //variables destinées à être utilisées pour envoi dans db
-        //deux cas possible avec et sans le fichier image
-        const userData = req.file
-          ? {
-              ...JSON.parse(req.body.userFile),
-              profilePic: `${req.protocol}://${req.get("host")}/images/${
-                req.file.filename
-              }`,
-            }
-          : { ...JSON.parse(req.body.userFile) };
-        console.log("USERDATA");
-        console.log(userData);
+          //variables destinées à être utilisées pour envoi dans db
+          //deux cas possible avec et sans le fichier image
+          const userData = req.file
+            ? {
+                ...JSON.parse(req.body.userFile),
+                profilePic: `${req.protocol}://${req.get("host")}/images/${
+                  req.file.filename
+                }`,
+              }
+            : { ...JSON.parse(req.body.userFile) };
+          console.log("USERDATA");
+          console.log(userData);
 
-        //update de la db
+          //update de la db
 
-        /*
+          /*
         UPDATE
     `user_file`
 SET
@@ -159,14 +159,14 @@ SET
 WHERE
     `id_user_file` = 15
         */
-        const { userId, lastName, firstName, age, profilePic } = userData;
-        console.log(
-          "CONSOLE LOG DE CA: userId, lastName, firstName, age, profilePic"
-        );
-        console.log(userId, lastName, firstName, age, profilePic);
+          const { userId, lastName, firstName, age, profilePic } = userData;
+          console.log(
+            "CONSOLE LOG DE CA: userId, lastName, firstName, age, profilePic"
+          );
+          console.log(userId, lastName, firstName, age, profilePic);
 
-        sqlUpdate = req.file
-          ? `
+          sqlUpdate = req.file
+            ? `
         UPDATE user_file SET
         user_file_lastName = ?,
         user_file_firstName = ?,
@@ -174,7 +174,7 @@ WHERE
         user_file_profilePic = ?
         WHERE id_user_file = ?
         `
-          : `
+            : `
         UPDATE user_file SET
         user_file_lastName = ?,
         user_file_firstName = ?,
@@ -182,22 +182,28 @@ WHERE
         WHERE id_user_file = ?
         `;
 
-        const values = req.file
-          ? [lastName, firstName, age, profilePic, id]
-          : [lastName, firstName, age, id];
+          const values = req.file
+            ? [lastName, firstName, age, profilePic, id]
+            : [lastName, firstName, age, id];
 
-        console.log("VALUES");
-        console.log(values);
+          console.log("VALUES");
+          console.log(values);
 
-        db.query(sqlUpdate, values, (error, results) => {
-          if (error) {
-            res.status(500).json({ error });
-          } else {
-            res
-              .status(201)
-              .json({ message: "user file updated in db", results });
-          }
-        });
+          db.query(sqlUpdate, values, (error, results) => {
+            if (error) {
+              res.status(500).json({ error });
+            } else {
+              res
+                .status(201)
+                .json({ message: "user file updated in db", results });
+            }
+          });
+        } else {
+          console.log("USERID DIFFERENT DE L'USERID DANS L'OBJET");
+          res
+            .status(403)
+            .json({ message: "you are not allowed to modify data" });
+        }
       }
     });
   } catch (error) {
