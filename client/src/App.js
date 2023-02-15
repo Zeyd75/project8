fae4861897307d42e60b19d0ef056bf9280c5297
Zeyd Haigar <zeyd.haigar@gmail.com>
2023-02-15 10:50:12 +0100

    intégration font awesome ok

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,12 @@
+import Header from "./components/Header";
+
 function App() {
-  return <div className="App">YO</div>;
+  return (
+    <div className="App">
+      YO
+      <Header />
+    </div>
+  );
 }
 
 export default App;
---
+++ client/src/components/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <>
+      <div className="header-container">
+        <h1>HEADER</h1>
+      </div>
+      <h1>test 1</h1>
+      <p>1 REM = 10 pixel</p>
+      <i className="fas fa-futbol"></i>{" "}
+    </>
+  );
+};
+
+export default Header;
